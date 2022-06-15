5e54a9ff8eabf1f275f5f29fb4f344fc7089a9e1
Jaspreet <singhrajpaljaspreet@gmail.com>
2022-06-15 12:29:08 +0530

    init

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Input = (name, label, onChange, value) => {
     return <div className="form-group">
-    <label htmlFor="name">{ label }</label>
+    <label htmlFor={name}>{ label }</label>
     <input
         onChange={onChange}
         value={value} 
