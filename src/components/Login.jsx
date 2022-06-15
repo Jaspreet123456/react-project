3b0db9ea88864a6c57531e74231f09f97de38b44
Jaspreet <singhrajpaljaspreet@gmail.com>
2022-06-15 16:45:54 +0530

    i

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Input from './input';
-import { Joi } from 'joi-browser';
+import  Joi  from 'joi-browser';
 
 class Login extends Component {
     state = {
@@ -9,31 +9,23 @@ class Login extends Component {
     };
 
     schema = {
-        username: Joi.string().required(),
-        password: Joi.string().required()
+        username: Joi.string().required().label("Username"),
+        password: Joi.string().required().label("Password")
     };
 
     validate = () => {
-        const result = Joi.validate(this.state.account, this.schema, {
-            abortEarly: false
-        });
-        console.log(result);
-        const errors ={};
-        const { account } = this.state;
-        if(account.username.trim() === '')
-            errors.username = "Username is required";
-        if(account.password.trim() === '')
-            errors.password = "Password is required";
-        
-        return Object.keys(errors).length === 0 ? null : errors;
+        const { options } = {abortEarly : false}
+        const error = Joi.validate(this.state.account, this.schema, options);
+        if (!error) return null;
+        const errors = {};
+        for(let item of error.details) errors[item.path[0]] = item.message;
+        return errors;
     }
     validateProperty = (name, value) => {
-        if (name === 'username') {
-            if (value.trim() === '') return "Username is required";
-        }
-        if (name === 'password') {
-            if (value.trim() === '') return "Password is required";
-        }
+        const obj = { [name] : value};
+        const schema = {[name] : this.schema[name]};
+        const { error } = Joi.validate(obj, schema);
+        return error ? error.details[0].message : null;
     }
 
     handleSubmit = e => {
@@ -64,7 +56,6 @@ class Login extends Component {
                     value={account.username}
                     label="Username"
                     error={errors.username}
-                    type="text"
                 />
                 <Input
                     name="password"
@@ -72,7 +63,6 @@ class Login extends Component {
                     value={account.password}
                     label="Password"
                     error={errors.password}
-                    type="password"
                 />
                 <button className="btn btn-primary">Login</button>
             </form>
