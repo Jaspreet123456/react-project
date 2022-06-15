af6f15aaf138132b2f92d6c4947a474dc5d56638
Jaspreet <singhrajpaljaspreet@gmail.com>
2022-06-15 14:55:50 +0530

    init

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,38 +16,54 @@ class Login extends Component {
             errors.password = "Password is required";
 
         return Object.keys(errors).length === 0 ? null : errors;
+    };
+
+    validateProperty = (name, value) => {
+        if(name === 'username'){
+            if(value.trim() === '') return "Username is required";
+        }
+        if(name === 'password'){
+            if(value.trim() === '') return "Password is required";
+        }
     }
 
     handleSubmit = e => {
         e.preventDefault();
-        console.log(errors)
         const errors = this.validate();
-        this.setState({errors});
+        this.setState({errors : errors || {}});
         if (errors) return;
-        console.log("Submitted");
+        console.log("submitted")
     };
 
     handleChange = ({ currentTarget: input }) => {
+        const errors = { ...this.state.errors };
+        const errorMessage = this.validateProperty(input);
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name]
         const account = { ...this.state.account };
         account[input.name] = input.value;
-        this.setState({ account });
+        this.setState({ account, errors });
     }
     render() {
-        const { account } = this.state;
+        const { account, errors } = this.state;
         return <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
                 <Input
-                    name="sername"
+                    name="username"
                     onChange={this.handleChange}
                     value={account.username}
                     label="Username"
+                    error = {errors.username}
+                    type = "text"
                 />
                 <Input
                     name="password"
                     onChange={this.handleChange}
                     value={account.password}
                     label="Password"
+                    error = {errors.password}
+                    type = "password"
                 />
                 <button className="btn btn-primary">Login</button>
             </form>
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const Input = ({name, label, onChange, value}) => {
+const Input = ({name, label, onChange, error, type, value}) => {
     return (<div className="form-group">
     <label htmlFor={name}>{ label }</label>
     <input
         onChange={onChange}
         value={value} 
         name={name}
-        id={name} type="text" 
+        id={name} 
+        type={type} 
         className="form-control" />
+        {error && <div className="alert alert-danger">{error}</div>}
     </div>
+    
     );
 }
  
