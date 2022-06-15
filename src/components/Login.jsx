e41670d25e3d548e1d6f7e85b296cc2cf623d48c
Jaspreet <singhrajpaljaspreet@gmail.com>
2022-06-15 12:22:34 +0530

    INIT

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
+import Input from './input';
 
 class Login extends Component {
-    handleSubmit = e =>{
+    state = {
+        account: { username: "", password: "" }
+    }
+
+    handleSubmit = e => {
         e.preventDefault();
-        console.log("Submitted")
+        const username = this.username.current.value;
+        console.log("Submitted");
     };
+
+    handleChange = ({ currentTarget: input }) => {
+        const account = { ...this.state.account };
+        account[input.name] = input.value;
+        this.setState({ account });
+    }
     render() {
-    return <div>
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input id='username' type="text" className="form-control" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input id='password' type="text" className="form-control" />
-            </div>
-            <button className="btn btn-primary">Login</button>
-        </form>
-    </div>;
+        const { account } = this.state;
+        return <div>
+            <h1>Login</h1>
+            <form onSubmit={this.handleSubmit}>
+                <Input
+                    label="Username"
+                    onChange={this.handleChange}
+                    value={account.username}
+                    name="sername"
+                />
+                <Input
+                    label="Password"
+                    onChange={this.handleChange}
+                    value={account.password}
+                    name="password"
+                />
+                <button className="btn btn-primary">Login</button>
+            </form>
+        </div>;
     }
 }
 export default Login;
---
+++ src/components/input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Input = (name, label, onChange, value) => {
+    return <div className="form-group">
+    <label htmlFor="name">{label}</label>
+    <input
+    onChange={onChange}
+    value={value} 
+    name={name}
+    id={name} type="text" 
+    className="form-control" />
+    </div>;
+}
+ 
+export default Input;
